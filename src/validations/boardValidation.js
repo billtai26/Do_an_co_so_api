0fe28f335992160a2f68c81f7474da58a41830ab
billtai26 <anhtaipxuan@gmail.com>
2025-04-17 18:23:29 +0700

    Error Handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -5,6 +5,7 @@
  */
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   /**
@@ -25,15 +26,12 @@ const createNew = async (req, res, next) => {
   })
 
   try {
-    
     // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả lỗi
     await correctCondition.validateAsync(req.body, { abortEarly: false })
     // Validate dữ liệu xong xuôi hợp lệ thì cho request đi tiếp sang Controller
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
