2f038dac1bae5f5bfd295c4c8a08e09307c678ac
billtai26 <anhtaipxuan@gmail.com>
2025-04-29 21:53:17 +0700

    Update Whitelist Domains again 1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,7 +7,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
-  'https://do-an-co-so-one.vercel.app/'
+  'https://do-an-co-so-one.vercel.app'
   // ...vv ví dụ sau này sẽ deploy lên domain chính thức ...vv
 ]
 
